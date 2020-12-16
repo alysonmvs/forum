0e7470c927fe1100c2703ffd08271996aaf4d3bc
Alyson Mendes <alysonmvs@hotmail.com>
2020-12-16 02:09:41 -0300

    implementacao spring security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$sBMs/JcR5TyxZpFa0zkQA.ojnhsh7EzpTLTy4vJfa1Ilzye4XqlUm');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
